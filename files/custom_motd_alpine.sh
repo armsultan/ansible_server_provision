c135fbf0f1065073868daa2c00c08ba9dc69ee13
armsultan <armsultan@gmail.com>
2020-08-10 10:53:09 -0600

    add alpine

--- files/custom_motd_alpine.sh
+++ files/custom_motd_alpine.sh
@@ -19,7 +19,7 @@ cat > /etc/motd << EOF
  - Uptime..............: $UPTIME_DAYS days, $UPTIME_HOURS hours, $UPTIME_MINUTES minutes
  - Users...............: Currently `users | wc -w` user(s) logged on: `users | sed -E -e 's/[[:blank:]]+/, /g'`
 
- - Primary IP Address..: `hostname -I | awk '{print $1}'`
+ - Primary IP Address..: `ifconfig eth0 | grep "inet addr" |  awk -F: '{print $2}' | awk '{print $1}'`
  - Distro..............: `lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om`
  - Kernel..............: `uname -r`
  - CPU.................: `cat /proc/cpuinfo | grep 'model name' | head -1 | cut -d':' -f2 | sed 's/ //'`
